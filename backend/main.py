13baa59ffe6eb5e20e1e6ad4f1a2011acab41949
Bllyth <roathdoug@gmail.com>
2021-06-22 20:19:24 +0300

    Added database

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 
+from app.db import database, User
+
 app = FastAPI()
 
 
@@ -8,3 +10,17 @@ async def root():
     return {
         "message": "Welcome to vercel"
     }
+
+
+@app.on_event('startup')
+async def startup():
+    if not database.is_connected():
+        await database.connect()
+
+    await User.objects.get_or_create(email="test@test.com")
+
+
+@app.on_event('shutdown')
+async def shutdown():
+    if database.is_connected:
+        await database.disconnect()
