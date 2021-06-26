a7466fd3bc75d75cca66a3007356bc1cd5f33bc8
Bllyth <roathdoug@gmail.com>
2021-06-27 01:49:35 +0300

    Added poetry

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,30 +1,10 @@
-from typing import Optional, Dict, Any
+import os
 
-from pydantic import BaseSettings, Field, PostgresDsn, validator
+from pydantic import BaseSettings, Field
 
 
 class Settings(BaseSettings):
     db_url: str = Field(..., env='DATABASE_URL')
-
-    # db_url: PostgresDsn = 'postgres:postgres@db:5432/lms-app'
-
-    # POSTGRES_SERVER: str
-    # POSTGRES_USER: str
-    # POSTGRES_PASSWORD: str
-    # POSTGRES_DB: str
-    # SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
-    #
-    # @validator("SQLALCHEMY_DATABASE_URI", pre=True)
-    # def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
-    #     if isinstance(v, str):
-    #         return v
-    #     return PostgresDsn.build(
-    #         scheme="postgresql",
-    #         user=values.get("POSTGRES_USER"),
-    #         password=values.get("POSTGRES_PASSWORD"),
-    #         host=values.get("POSTGRES_SERVER"),
-    #         path=f"/{values.get('POSTGRES_DB') or ''}",
-    #     )
 
 
 settings = Settings()
