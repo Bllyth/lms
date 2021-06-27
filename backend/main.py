a1e4c13947f7828e4ce249c99fa6d2a37946828e
Bllyth <roathdoug@gmail.com>
2021-06-27 04:25:45 +0300

    Added poetry

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from app.db import database, User
+from app.db import User, database
 
 app = FastAPI()
 
@@ -12,15 +12,15 @@ async def root():
     }
 
 
-# @app.on_event('startup')
-# async def startup():
-#     if not database.is_connected():
-#         await database.connect()
-#
-#     await User.objects.get_or_create(email="test@test.com")
-#
-#
-# @app.on_event('shutdown')
-# async def shutdown():
-#     if database.is_connected:
-#         await database.disconnect()
+@app.on_event('startup')
+async def startup():
+    if not database.is_connected():
+        await database.connect()
+
+    await User.objects.get_or_create(email="test@test.com")
+
+
+@app.on_event('shutdown')
+async def shutdown():
+    if database.is_connected:
+        await database.disconnect()
