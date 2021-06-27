56699c9f120df28ab710d1dd997fa5ae48607495
Bllyth <roathdoug@gmail.com>
2021-06-27 04:46:30 +0300

    Added poetry

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,32 @@
+import databases
+import ormar
+import sqlalchemy
 from fastapi import FastAPI
 
-from backend.app.db import database, User
+from app.config import settings
 
 app = FastAPI()
+
+database = databases.Database(settings.db_url)
+metadata = sqlalchemy.MetaData()
+
+
+class BaseMeta(ormar.ModelMeta):
+    metadata = metadata
+    database = database
+
+
+class User(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "users"
+
+    id: int = ormar.Integer(primary_key=True)
+    email: str = ormar.String(max_length=128, unique=True, nullable=False)
+    active: bool = ormar.Boolean(default=True, nullable=False)
+
+
+engine = sqlalchemy.create_engine(settings.db_url)
+metadata.create_all(engine)
 
 
 @app.get('/')
