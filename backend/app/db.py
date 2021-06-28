e85e687bbc306a0cacf8283ebe3d8f526dc894dd
Bllyth <roathdoug@gmail.com>
2021-06-28 21:19:13 +0300

    Added Auth

--- backend/app/db.py
+++ backend/app/db.py
@@ -3,7 +3,7 @@ import sqlalchemy
 from .base import metadata
 from .config import settings
 
-from auth.models import User
+from .auth.models import User
 
 engine = sqlalchemy.create_engine(settings.DB_URL)
 metadata.create_all(engine)
