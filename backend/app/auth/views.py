b36c8a2d0c15d076a3d391632289dd77bff01c63
Bllyth <roathdoug@gmail.com>
2021-07-10 21:56:20 +0300

    Added migrations

--- backend/app/auth/views.py
+++ backend/app/auth/views.py
@@ -11,6 +11,12 @@ async def login(username: str = Form(...), password: str = Form(...)):
     return {"username": username}
 
 
+@user_router.post('/add_user', response_model=User)
+async def add_user(user: User):
+    await user.save()
+    return user
+
+
 @user_router.get('/{user_id}', response_model=User)
 async def get_user(user_id: int):
     user_db = await User.objects.get(id=user_id)
