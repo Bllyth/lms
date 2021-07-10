12b8b39768bd40216f35cec87a45ed19e61fac64
Bllyth <roathdoug@gmail.com>
2021-07-10 15:24:19 +0300

    Disabled HTTPS

--- lms-master/backend/app/auth/service.py
+++
@@ -1,6 +0,0 @@
-# from .models import User
-#
-#
-# def get():
-#     # Returns a user based on given user id.
-#     return await User.
--- lms-master/backend/app/db.py
+++
@@ -1,9 +0,0 @@
-import sqlalchemy
-
-from .base import metadata
-from .config import settings
-
-from .auth.models import User
-
-engine = sqlalchemy.create_engine(settings.DB_URL)
-metadata.create_all(engine)
